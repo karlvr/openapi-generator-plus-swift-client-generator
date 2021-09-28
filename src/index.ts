00fe89dbbd9f9bcbca6e9c997673d1cbebd0177a
Karl von Randow <karl@xk72.com>
2021-09-29 12:05:51 +1300

    Reference Foundation and Swift things using the module name to avoid conflicts

--- src/index.ts
+++ src/index.ts
@@ -85,7 +85,8 @@ const RESERVED_WORDS = [
 	'internal', 'is', 'lazy', 'left', 'let', 'mutating', 'nil', 'none', 'nonmutating', 'open', 'operator', 'optional', 'override', 'postfix', 'precedence', 'prefix', 'private',
 	'protocol', 'public', 'repeat', 'required', 'rethrows', 'return', 'right', 'self', 'set', 'static', 'struct', 'subscript', 'super', 'switch', 'throw', 'throws', 'true',
 	'try', 'typealias', 'unowned', 'var', 'weak', 'where', 'while', 'willSet',
-	'LocalDate', 'LocalTime', 'OffsetDateTime', 'Decimal', 'String', 'unknown',
+	'LocalDate', 'LocalTime', 'OffsetDateTime', 'Decimal', 'String', 'Void',
+	'unknown', // for our enum cases
 ]
 
 export function options(config: CodegenConfig, context: SwiftGeneratorContext): CodegenOptionsSwift {
