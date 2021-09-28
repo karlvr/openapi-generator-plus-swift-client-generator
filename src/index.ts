ae523a5b6dc68b487e77979d6aadb6a0721da805
Karl von Randow <karl@xk72.com>
2021-09-29 10:43:36 +1300

    Support binary strings

--- src/index.ts
+++ src/index.ts
@@ -218,6 +218,9 @@ export default function createGenerator(config: CodegenConfig, context: SwiftGen
 				case CodegenSchemaType.DATETIME:
 					return new context.NativeType('OffsetDateTime')
 				case CodegenSchemaType.STRING:
+					if (format === 'binary') {
+						return new context.NativeType('Data')
+					}
 					return new context.NativeType('String')
 				case CodegenSchemaType.BOOLEAN:
 					return new context.NativeType('Bool')
