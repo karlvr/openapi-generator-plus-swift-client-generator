6acd11eb4fa104d9e329a385603dc7b9873e9f39
Karl von Randow <karl@xk72.com>
2021-09-29 11:21:40 +1300

    testing: test using specs from main generators branch

--- src/__tests__/compile.spec.ts
+++ src/__tests__/compile.spec.ts
@@ -5,20 +5,29 @@ import path from 'path'
 import { configObject } from '@openapi-generator-plus/generator-common'
 
 describe('compile test cases', () => {
-	const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
-	const files = fs.readdirSync(basePath)
-
-	for (const file of files) {
-		test(file, async() => {
-			const result = await prepare(path.join(basePath, file), {
-				...DEFAULT_CONFIG,
-				npm: {
-					...configObject(DEFAULT_CONFIG, 'npm', {}),
-					name: path.basename(file),
-				},
-				includeTests: true,
+	function compileFiles(basePath: string, files: string[]) {
+		for (const file of files) {
+			test(file, async() => {
+				const result = await prepare(path.join(basePath, file), {
+					...DEFAULT_CONFIG,
+					npm: {
+						...configObject(DEFAULT_CONFIG, 'npm', {}),
+						name: path.basename(file),
+					},
+					includeTests: true,
+				})
+				await testGenerate(result, build, path.join('test-output', file))
 			})
-			await testGenerate(result, build, path.join('test-output', file))
-		})
+		}
+	}
+
+	const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
+	if (fs.existsSync(basePath)) {
+		compileFiles(basePath, fs.readdirSync(basePath))
+	}
+
+	const sharedBasePath = path.join(__dirname, '../../../openapi-generator-plus-generators/__tests__/specs')
+	if (fs.existsSync(sharedBasePath)) {
+		compileFiles(sharedBasePath, fs.readdirSync(sharedBasePath))
 	}
 })
