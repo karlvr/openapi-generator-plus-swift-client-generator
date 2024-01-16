dac97279fd7a5d636a742cc2024717aa3e80b601
Karl von Randow <karl@cactuslab.com>
2024-01-16 13:31:13 +1300

    test: fix local compile test

--- src/__tests__/compile.spec.ts
+++ src/__tests__/compile.spec.ts
@@ -24,6 +24,8 @@ describe('compile test cases', () => {
 	const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
 	if (fs.existsSync(basePath)) {
 		compileFiles(basePath, fs.readdirSync(basePath))
+	} else {
+		console.warn(`Cannot find __tests__ in local repo: ${basePath}`)
 	}
 
 	const sharedBasePath = path.join(__dirname, '../../../openapi-generator-plus-generators/__tests__/specs')
