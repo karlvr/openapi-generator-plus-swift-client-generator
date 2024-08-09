71b15216c84e3a77b8229dbec65667fa59b6e226
TomCarey <tom@cactuslab.com>
2024-08-09 14:12:52 +1200

    Added reserved keyword

--- src/index.ts
+++ src/index.ts
@@ -90,6 +90,7 @@ const RESERVED_WORDS = [
 	'RetryConfiguration', 'Configuration',
 	'SecurityClient', 'SecurityClientController', 'SecurityScheme', 'OAuthPasswordFlowClient', 'OAuthClientCredentialsFlowClient', 'OAuthAuthorizationCodeFlowClient', 
 	'OAuthAccessTokenManager', 'AccessTokenHandler', 'OAuthAccessToken', 'BasicAuthenticationSecurityClient', 'APIKeySecurityClient', 'AbstractOAuthFlowClient',
+	'OAuthConfiguration',
 ]
 
 export function options(config: CodegenConfig, context: SwiftGeneratorContext): CodegenOptionsSwift {
