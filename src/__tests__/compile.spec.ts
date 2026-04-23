3c2da59aef88cd6402511c0c099c0fc8ec296fdf
Karl von Randow <karl@cactuslab.com>
2026-04-23 14:14:18 +1200

    fix: tests need to support dirs in specs

--- src/__tests__/compile.spec.ts
+++ src/__tests__/compile.spec.ts
@@ -3,9 +3,11 @@ import { build, prepare, DEFAULT_CONFIG } from './common'
 import fs from 'fs'
 import path from 'path'
 import { configObject } from '@openapi-generator-plus/generator-common'
+import { globSync } from 'glob'
 
 describe('compile test cases', () => {
-	function compileFiles(basePath: string, files: string[]) {
+	function compileFiles(basePath: string) {
+		const files = globSync('**/*.{yml,yaml}', { cwd: basePath })
 		for (const file of files) {
 			test(file, async() => {
 				const result = await prepare(path.join(basePath, file), {
@@ -23,18 +25,18 @@ describe('compile test cases', () => {
 
 	const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
 	if (fs.existsSync(basePath)) {
-		compileFiles(basePath, fs.readdirSync(basePath))
+		compileFiles(basePath)
 	} else {
 		console.warn(`Cannot find __tests__ in local repo: ${basePath}`)
 	}
 
 	const sharedBasePath = path.join(__dirname, '../../../openapi-generator-plus-generators/__tests__/specs')
 	if (fs.existsSync(sharedBasePath)) {
-		compileFiles(sharedBasePath, fs.readdirSync(sharedBasePath))
+		compileFiles(sharedBasePath)
 	}
 
 	const ciSharedBasePath = path.join(__dirname, '../../test-input/__tests__/specs')
 	if (fs.existsSync(ciSharedBasePath)) {
-		compileFiles(ciSharedBasePath, fs.readdirSync(ciSharedBasePath))
+		compileFiles(ciSharedBasePath)
 	}
 })
